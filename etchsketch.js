6a15f31cdb4e84d04d11990e5d1bca4cdb6fede3
DiegoSer23 <dservin2011@hotmail.com>
2026-03-09 21:32:43 -0600

    Added hover functionality

--- etchsketch.js
+++ etchsketch.js
@@ -1,9 +1,22 @@
 const gridContainer = document.getElementById("container");
+const buttonGrid = document.getElementById("grid-size");
+
+buttonGrid.addEventListener("click", handleGridSize);
 
 for (let i = 0; i < 16; i++) {
     let gridSquare = document.createElement("div");
     gridSquare.className = "grid";
     gridSquare.style.backgroundColor = "lightblue";
     gridSquare.style.padding = "5px";
+    gridSquare.addEventListener("mouseenter", handleMouseEnter);
     gridContainer.appendChild(gridSquare);
+}
+
+function handleMouseEnter(event) {
+    this.style.backgroundColor = "lightcoral";
+}
+
+function handleGridSize(event) {
+    let gridSize = prompt("Input grid size:", 16);
+    
 }
